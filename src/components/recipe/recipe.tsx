028e9f2675ed8194ad82f49a33d4a74d205ca07d
Xabi <xabicasadonieto@gmail.com>
2023-12-12 15:23:00 +0100

    Hide copy button

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -1,28 +1,28 @@
 'use client'
 
 import { type RecipeType } from '@/types/recipe.types'
-import { Button } from '@/components/button'
+// import { Button } from '@/components/button'
 import { Ingredients } from './ingredients'
 import { Steps } from './steps'
 import { Tips } from './tips'
 import { RecipeTitle } from './recipe.styled'
-import { generateRecipeText } from './recipe.utils'
+// import { generateRecipeText } from './recipe.utils'
 
 export function Recipe(props: RecipeType) {
   const { title, ingredients, steps, tips } = props
 
-  const handleClipboard = () => {
-    const recipeText = generateRecipeText(props)
+  // const handleClipboard = () => {
+  //   const recipeText = generateRecipeText(props)
 
-    navigator.clipboard.writeText(recipeText)
-  }
+  //   navigator.clipboard.writeText(recipeText)
+  // }
 
   return (
     <>
       <RecipeTitle>{title}</RecipeTitle>
 
-      <br />
-      <Button label={'Copiar'} onClick={handleClipboard} />
+      {/* <br /> */}
+      {/* <Button label={'Copiar'} onClick={handleClipboard} /> */}
 
       <br />
 
