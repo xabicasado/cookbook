58cb8c237dbf2428a0c1ea6758f3434e0eab5304
Xabi <xabicasadonieto@gmail.com>
2023-12-14 22:51:09 +0100

    RecipeProvider misplaced

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -1,25 +1,24 @@
 'use client'
 
 import { type RecipeType } from '@/types/recipe.types'
-import { Button } from '@/components/button'
+// import { Button } from '@/components/button'
 import { Ingredients } from './ingredients'
 import { Steps } from './steps'
 import { Tips } from './tips'
-import { generateRecipeText } from './recipe.utils'
-import { RecipeProvider } from '@/contexts/recipe.context'
+// import { generateRecipeText } from './recipe.utils'
 import { PageContentStyled } from '@/app/layout.styled'
 
 export function Recipe(props: RecipeType) {
   const { title, ingredients, steps, tips } = props
 
-  const handleClipboard = () => {
-    const recipeText = generateRecipeText(props)
+  // const handleClipboard = () => {
+  //   const recipeText = generateRecipeText(props)
 
-    navigator.clipboard.writeText(recipeText)
-  }
+  //   navigator.clipboard.writeText(recipeText)
+  // }
 
   return (
-    <RecipeProvider value={{}}>
+    <>
       <h2>{title}</h2>
       <PageContentStyled>
         {/* <Button label={'Copiar'} onClick={handleClipboard} /> */}
@@ -30,6 +29,6 @@ export function Recipe(props: RecipeType) {
 
         {!(tips == null) && <Tips tips={tips} />}
       </PageContentStyled>
-    </RecipeProvider>
+    </>
   )
 }
