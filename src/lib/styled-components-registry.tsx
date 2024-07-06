200fedfacbb1716f7bdda5317da0faeca86d956b
Xabi <xabicasadonieto@gmail.com>
2024-07-06 08:31:14 +0200

    Changed styled-components config

--- src/lib/styled-components-registry.tsx
+++ src/lib/styled-components-registry.tsx
@@ -1,14 +1,9 @@
-'use client'
-
-import React, { useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
+import React, { useState } from 'react'
+
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
@@ -27,3 +22,5 @@ export default function StyledComponentsRegistry({
     </StyleSheetManager>
   )
 }
+
+export default StyledComponentsRegistry
