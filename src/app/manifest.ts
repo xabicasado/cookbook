d8a81d08b9de9b54c274be5838905c3403725ffd
Xabi <xabicasadonieto@gmail.com>
2024-10-21 08:29:49 +0200

    fix: maskable icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,8 +16,14 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     icons: [
       {
-        src: '/icon.png',
+        src: '/icon1.png',
         sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon2.png',
+        sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
