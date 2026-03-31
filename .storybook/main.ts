d651fccfd82c2b9861e45aa5923b2508cac6333e
Xabi <xabicasadonieto@gmail.com>
2026-03-31 19:41:26 +0200

    chore: upgraded and fixed dependencies, fix storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,33 @@
+import { mergeConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 import type { StorybookConfig } from '@storybook/nextjs-vite'
 
 const config: StorybookConfig = {
   stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
-    '@storybook/addon-vitest',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
   ],
   framework: '@storybook/nextjs-vite',
   staticDirs: ['../public'],
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+      build: {
+        rollupOptions: {
+          onwarn(
+            warning: { code?: string; message: string },
+            warn: (w: { message: string }) => void
+          ) {
+            if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+            warn(warning)
+          },
+        },
+      },
+    })
+  },
 }
 export default config
